a9040c5d93f24e5bcacc6b411cc293ae255f5e23
Thanh Long <occurmikey@example.com>
2025-05-28 00:54:50 +0700

    Long_1 : Set up DB & Programs

---
+++ NewManagementSystem/Configuration/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿namespace NewManagementSystem.Configuration
+{
+	public static class ServiceRegistration
+	{
+		// Phương thức mở rộng để đăng ký các service
+		public static void ConfigureServices(WebApplicationBuilder builder)
+		{
+			// Đăng ký các repository
+
+
+			// Đăng ký các Service
+
+
+		}
+	}
+}
--- NewManagementSystem/Program.cs
+++ NewManagementSystem/Program.cs
@@ -1,27 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using NewManagementSystem.Configuration;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace NewManagementSystem
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Đăng ký controller
+			builder.Services.AddControllersWithViews();
+
+			// Gọi method cấu hình services trong class ServiceRegistration
+			ServiceRegistration.ConfigureServices(builder);
+
+			var app = builder.Build();
+
+			// Middleware pipeline
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
