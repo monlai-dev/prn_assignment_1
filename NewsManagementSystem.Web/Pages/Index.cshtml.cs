851c78402846e36cba6fc7f158c98c33369446e5
Thanh Long <occurmikey@example.com>
2025-06-20 15:55:15 +0700

    Long_2 : Full Login + regis + home

--- NewsManagementSystem.Web/Pages/Index.cshtml.cs
+++ NewsManagementSystem.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewManagementSystem.Models;
+using NewManagementSystem.Services.Abstractions;
+using NewsManagementSystem.BusinessObject.Configuration;
+using NewsManagementSystem.Services.Services.Abstractions;
 
 namespace NewsManagementSystem.Web.Pages
 {
 	public class IndexModel : PageModel
 	{
-		private readonly ILogger<IndexModel> _logger;
+		private readonly IAccountService _accountService;
+		private readonly INewsService _newsService;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(IAccountService accountService, INewsService newsService)
 		{
-			_logger = logger;
+			_accountService = accountService;
+			_newsService = newsService;
 		}
 
-		public void OnGet()
-		{
+		public List<NewsArticle> NewsList { get; set; } = new();
+		public LoginDTO? UserInfo { get; set; }
+		public int CurrentPage { get; set; } = 1;
+		public int TotalPages { get; set; } = 1;
+		public int PageSize { get; set; } = 4;
 
+		public async Task<IActionResult> OnGetAsync(int page = 1)
+		{
+			CurrentPage = page;
+
+			if (User.Identity?.IsAuthenticated ?? false)
+			{
+				var user = await _accountService.FindAccountByUserName(User.Identity.Name);
+				ViewData["UserInfo"] = new LoginDTO
+				{
+					AccountName = user.AccountName,
+					AccountEmail = user.AccountEmail
+				};
+			}
+
+			var allNews = _newsService.GetAllNewsWithDetails().ToList();
+
+			int totalNews = allNews.Count;
+			TotalPages = (int)Math.Ceiling(totalNews / (double)PageSize);
+
+			NewsList = allNews
+				.Skip((CurrentPage - 1) * PageSize)
+				.Take(PageSize)
+				.ToList();
+
+			return Page();
 		}
 	}
 }
---
+++ NewsManagementSystem.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsManagementSystem.Web.ViewModels
+{
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage = "Email không được để trống")]
+		[EmailAddress(ErrorMessage = "Email không hợp lệ")]
+		public string? LoginEmail { get; set; }
+
+		[Required(ErrorMessage = "Mật khẩu không được để trống")]
+		public string? LoginPassword { get; set; }
+	}
+}
